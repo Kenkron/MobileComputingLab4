43ae127b8c8b18304a1855dd48bcf1f147a9a6f5
Christopher Waugh <kenkron@gmail.com>
2017-03-19 20:43:54 -0400

    Trying BLE

--- Heatmap/app/src/main/java/uf/cnt5517/g21/heatmap/MainActivity.java
+++ Heatmap/app/src/main/java/uf/cnt5517/g21/heatmap/MainActivity.java
@@ -1,6 +1,14 @@
 package uf.cnt5517.g21.heatmap;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -22,8 +30,25 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     public static final String TAG = "Heatmap.MainActivity";
 
+    private final static int REQUEST_ENABLE_BT = 1;
+    private final static int SCAN_PERIOD = 3000;
+
     GoogleApiClient mGoogleApiClient;
     Networking broker;
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    private BluetoothLeScanner beaconFinder;
+
+    // Initializes Bluetooth adapter.
+    BluetoothManager bluetoothManager;
+
+    // Device scan callback.
+    private ScanCallback mLeScanCallback;
+
+
+    private boolean mScanning;
+    private Handler mHandler;
 
     public void toast(String text){
         Context context = getApplicationContext();
@@ -45,6 +70,52 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .build();
 
         broker = new Networking(this, "http://192.168.0.30:8080", "Android_"+System.currentTimeMillis());
+
+
+        bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        beaconFinder = bluetoothAdapter.getBluetoothLeScanner();
+
+        mHandler = new Handler();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        mLeScanCallback =
+                new ScanCallback() {
+                    @Override
+                    public void onScanResult(int callbackType, ScanResult result) {
+                        MainActivity.this.toast("I Here's what I found: " + result);
+                    }
+                };
+
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    beaconFinder.stopScan(mLeScanCallback);
+                    MainActivity.this.toast("I couldn't find anything");
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            beaconFinder.startScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            beaconFinder.stopScan(mLeScanCallback);
+        }
     }
 
     @Override
@@ -75,5 +146,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         toast("[onConnectionFailed] Result: " + connectionResult);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scanLeDevice(false);
+        toast("scanning");
     }
 }
